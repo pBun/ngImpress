168217c72cfafe6b8a527f4728244ac58eb532a1
Dustin Boersma <dustin.boersma@gmail.com>
2014-11-11 17:08:09 -0800

    cleaned up stuff

--- src/javascript/app/impressDemo.controller.js
+++ src/javascript/app/impressDemo.controller.js
@@ -4,13 +4,7 @@ var controller = function($scope, $sce) {
 
   this.scope = $scope;
 
-  this.scope.message = $sce.trustAsHtml('My name is Dustin.<br/> Here are a few reasons why you should hire me.');
-
 };
 controller.$inject = ['$scope', '$sce'];
-
-controller.prototype.doSomething = function() {
-  console.log('test controller did something!')
-};
 
 module.exports = controller;
